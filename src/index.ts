dc9005666eb8b178c2fdf21f969545eef55ad2c4
Griffithtp <griffithtp@gmail.com>
2020-05-22 15:28:48 +0100

    improve types

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,72 @@
-let topics: Record<string, unknown> = {};
-// type Record<K extends string | number | symbol, T> = { [P in K]: T; }
+interface IListenerCallback {
+  (data: ICallbackData): any;
+}
 
-function subscribe(topic: string, listener: ({}) => {}) {
+interface ICallbackData {
+  topic: string;
+  value: any;
+  eventObject: any;
+}
+
+let topics: Record<string, unknown> = {};
+
+function subscribe(topic: string, listener: IListenerCallback) {
   if (!topics.hasOwnProperty(topic)) {
     topics[topic] = listener;
   }
   window.addEventListener(
     topic,
-    eventData => processCustomEvent(topic, listener, eventData),
+    (eventData) => processCustomEvent(topic, listener, eventData),
     false
   );
   window.addEventListener(
     "storage",
-    eventData => processStorage(topic, listener, eventData),
+    (eventData) => processStorage(topic, listener, eventData),
     false
   );
   return {
     unsubscribe: () => {
       unsubscribe(topic);
-    }
+    },
   };
 }
 
-function processCustomEvent(topic: string, cb: ({}) => {}, result: any) {
+function processCustomEvent(topic: string, cb: IListenerCallback, result: any) {
   if (topic === result.type) {
-    return cb({ topic, value: result.detail, eventObject: result });
+    return cb({
+      topic,
+      value: result.detail,
+      eventObject: result,
+    } as ICallbackData);
   }
 }
 
-function processStorage(topic: string, cb: ({}) => {}, result: any) {
+function processStorage(topic: string, cb: IListenerCallback, result: StorageEvent) {
   if (topic === result.key) {
-    return cb({ topic, value: result.newValue, eventObject: result });
+    return cb({
+      topic,
+      value: result.newValue,
+      eventObject: result,
+    } as ICallbackData);
   }
 }
 
-function publish(eventName: string, eventValue: any) {
-  if (topics[eventName]) {
-    window.localStorage.setItem(eventName, eventValue);
-    const customEvent = new CustomEvent(eventName, { detail: eventValue });
+function publish(topic: string, message: any) {
+  if (topics[topic]) {
+    window.localStorage.setItem(topic, message);
+    const customEvent = new CustomEvent(topic, { detail: message });
     window.dispatchEvent(customEvent);
   }
 }
 
-function unsubscribe(subscribeKey: string) {
-  window.localStorage.removeItem(subscribeKey);
-  delete topics[subscribeKey];
+function unsubscribe(topic: string) {
+  window.localStorage.removeItem(topic);
+  delete topics[topic];
 }
 
 export default {
   topics,
   publish,
   subscribe,
-  unsubscribe
+  unsubscribe,
 };
